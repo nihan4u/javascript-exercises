701a3eb9b9c3c87b2ad96548792d16caca1ea10a
nihan4u <nowa.uddin@gmail.com>
2024-03-27 22:21:20 -0400

    Finish 03reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    if (typeof word !== `string`) {
+        return "Invalid Input";
+    }  
+    let reversedWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversedWord += word[i];
+    }
+    return reversedWord;
 };
 
+/* Alternative Solution
+const reverseString = function(word) {
+    if (typeof word !== 'string') return 'Invalid Input';
+    return word.split('').reverse().join('');
+};
+*/
 // Do not edit below this line
 module.exports = reverseString;
