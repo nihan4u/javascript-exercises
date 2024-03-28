3fd831a465c877c89fbef787654a1b1c0b5c048e
nihan4u <nowa.uddin@gmail.com>
2024-03-28 05:22:13 -0400

    Finsih 04removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...indices) {
+    let newArray = []
+    array.forEach((item) => {
+        if (!indices.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
+
+/* Attempt 1
+const removeFromArray = function(array, indices) {
+    for (let i = 0; i < array.length; i++) {
+        if (indices.includes(i)) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
+};*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
